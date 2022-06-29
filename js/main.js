7d1a965b4e344418ebe44c4d90b212418150b72b
BYA <bya@BYAs-MacBook-Pro.local>
2022-06-29 11:15:45 +0300

    Add Prograess Bar, Stats Section and Countdown Timer for Events Section Animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+// OUR SKILLS Width On Scrolling
+//skills section
+let ourSkillsSection = document.querySelector(".our-skills");
+let spans = document.querySelectorAll(".the-progress span");
+let skills = document.querySelectorAll(".skills .skillP");
+let ourSkillsPercentageSection = document.querySelector(".our-skills");
+let SkillStarted = false; // Function Started ? NO
+//Stats Section
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? NO
+window.onscroll = function () {
+  //For progrees bar and skills persentage
+  if (window.scrollY >= ourSkillsSection.offsetTop) {
+    //For prograss bar
+    console.log("Reached Skill Section");
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+    //For skills persentage
+    if (!SkillStarted) {
+      skills.forEach((num) => startCount(num));
+    }
+    SkillStarted = true;
+  }
+  //Our Awsome Stats section
+  if (window.scrollY >= statsSection.offsetTop) {
+  if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count)
+    }
+  }, 2000 / goal);
+}
+
+
 // The End Of the year Date to countdown to
 // 1000 milliseconds = 1 sec
 
@@ -35,3 +80,5 @@ let counter = setInterval(() => {
   }
 
 }, 1000);
+
+
